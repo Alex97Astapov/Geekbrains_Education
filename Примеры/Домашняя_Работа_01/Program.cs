7a36aac8ee5693d9ea8ab25430b1731c9e487597
Aleksandr <astapov.sascha@yandex.ru>
2023-02-14 13:08:02 +0300

    Добавили проет с первым домашним заданием

---
+++ Примеры/Домашняя_Работа_01/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = new Random().Next(1,96); 
+int b = new Random().Next(1,96);
+int c = new Random().Next(1,96);
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.Write("max = ");
+Console.WriteLine(max);

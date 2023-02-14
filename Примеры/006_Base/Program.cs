b73a9a4490a197b47fd9ac69dfd5c6ba50f9d8e4
Aleksandr <astapov.sascha@yandex.ru>
2023-02-14 12:35:34 +0300

    Добавили проет с присваиванием рандомных чисел и выбора максимального

---
+++ Примеры/006_Base/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = new Random().Next(1,96); 
+int b = new Random().Next(1,96);
+int c = new Random().Next(1,96);
+int d = new Random().Next(1,96); 
+int i = new Random().Next(1,96);
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (i > max) max = i;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(i);
+Console.Write("max = ");
+Console.WriteLine(max);

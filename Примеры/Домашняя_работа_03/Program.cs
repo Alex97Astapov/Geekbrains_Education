67381be6161acb39c484a4ad7b0b49c13b09f7d9
Aleksandr <astapov.sascha@yandex.ru>
2023-02-14 18:37:21 +0300

    Добавили проет со третьего домашнего задания

---
+++ Примеры/Домашняя_работа_03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = 1;
+
+if (b == 1) {
+   b = b + 1;
+  }
+  while (b <= a) {
+  Console.WriteLine (b);
+  b=b+2;
+  }

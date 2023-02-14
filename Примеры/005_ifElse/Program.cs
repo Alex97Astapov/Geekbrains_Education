e630bbe6883413f53158b6490c88eb8cc95648ba
Aleksandr <astapov.sascha@yandex.ru>
2023-02-14 12:26:21 +0300

    Добавили проект приветсвия определенного пользователя

---
+++ Примеры/005_ifElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "aleksandr") {
+   Console.WriteLine ("Good day, my Lord! ");
+}
+else
+{
+   Console.Write("Hello, ");
+   Console.WriteLine(username);
+}

947cd5de93a2375a3d7a74b6e383ca9488242a22
Aleksandr <astapov.sascha@yandex.ru>
2023-02-16 12:14:53 +0300

    Третье Д/З ко второй лекции

---
+++ Домашнее_задание_06/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int num = new Random().Next(1,8);
+Console.WriteLine (num);
+if (num == 6) {
+Console.WriteLine ($"{num} - Выходной день");
+}
+if (num == 7) {
+Console.WriteLine ($"{num} - Выходной день");
+}
+else
+{
+   Console.WriteLine ($"{num} - Не выходной день ");
+}

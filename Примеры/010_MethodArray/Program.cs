6b17a1b7dda20a134d217ccbef12f134e5a595a7
Aleksandr <astapov.sascha@yandex.ru>
2023-02-17 11:50:40 +0300

    Добавили проекты 009, 010, 011 после лекции. Разбирали массивы, функции

---
+++ Примеры/010_MethodArray/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = {18,27,36,45,54,63,27,81};
+int n = array.Length;
+int find = 27;
+int index = 0;
+
+while (index < n) {
+if (array[index] == find){
+   Console.WriteLine (index);
+   break;
+}
+   index = index + 1;
+}

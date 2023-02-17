b9650ac3b162135a5ffa7297cb16ba11c1effb18
Aleksandr <astapov.sascha@yandex.ru>
2023-02-17 10:25:46 +0300

    Добавили проект с 5 Домашним заданием

--- Домашняя_работа_04/Program.cs
+++ Домашняя_работа_04/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Clear();
-int num = new Random(). Next(100,1000);
+int num = new Random(). Next(100, 1000);
 int a1 = num % 100;
 int a2 = a1 / 10;
 Console.WriteLine($"Выводим {num} и {a2}");
---
+++ Домашняя_работа_05/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+string value = Console.ReadLine();
+int number = Convert.ToInt32(value);
+
+if ( number < 99 ) {
+   Console.WriteLine  ($"{number} - нет третьего числа");
+}
+while (number > 999) {
+   number = number / 10;
+}
+if (99 < number ) {
+   number = number % 10;
+   Console.WriteLine($"{number} - третье число");
+}

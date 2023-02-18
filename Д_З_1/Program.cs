c4a7a4a9ffb08d1532a4a5790776fc16c3b57e19
Aleksandr <astapov.sascha@yandex.ru>
2023-02-18 17:22:28 +0300

    Выполнил задание с поиском расстояния между точками в 3D простанстве

---
+++ Д_З_1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите любое пятизначное число: ");
+int i = int.Parse(Console.ReadLine());
+int[] massiv = int new[];
+int b = 0;
+for (b = 0; )
---
+++ Д_З_3/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите значение Х1:  ");
+double x1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите значение Y1:  ");
+double y1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите значение Z1:  ");
+double z1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите значение Х2:  ");
+double x2 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите значение Y2:  ");
+double y2 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите значение Z2:  ");
+double z2 = double.Parse(Console.ReadLine());
+
+double b = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+
+Console.WriteLine($"b={b:f4}");

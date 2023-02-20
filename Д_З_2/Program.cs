e9e80ebac91f18e957c887951fef003f21af4a8b
Aleksandr <astapov.sascha@yandex.ru>
2023-02-20 19:25:02 +0300

    Выполнил задачу с кубами

--- Д_З_2/Program.cs
+++ Д_З_2/Program.cs
@@ -1,1 +1,25 @@
 ﻿
+Console.Clear();
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int i = 1;
+int m = N;
+
+if (N < 0)
+{
+   i = N;
+   m = -N;
+   N = -1;
+}
+
+int[] cube = new int[m];
+for (int j = 0; i <= N; i++)
+{
+   cube[j] = i * i * i;
+   j++;
+}
+for (int j = 0; j < m; j++)
+{
+   Console.WriteLine($"{cube[j]}");
+}

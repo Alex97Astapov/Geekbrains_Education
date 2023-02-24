a5df9023e36b61e3395771411cc9571c0005d3c2
Aleksandr <astapov.sascha@yandex.ru>
2023-02-24 17:12:23 +0300

    Сделал первое задание к 4 уроку

---
+++ Домашняя_работа_4_урок(1)/Program.cs
@@ -0,0 +1,18 @@
+﻿int exp(int a, int b)
+{
+   int i = 1;
+   int k = a;
+   while (i < b)
+   {
+      k = k * a;
+      i++;
+   }
+   return k;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число а: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"Число {a} в степени {b} -> {exp(a, b)}");

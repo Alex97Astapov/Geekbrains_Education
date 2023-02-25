6b74e467a7f77cb7f84c50efc6f3dd594363ebc1
Aleksandr <astapov.sascha@yandex.ru>
2023-02-25 16:40:00 +0300

    Сделал второе и третье  задание к 4 уроку

---
+++ Домашняя_работа_04_урок(3)/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] exp(int size)
+{
+   int[] col = new int[size];
+   for (int i = 0; i < size; i++)
+   {
+      col[i] = new Random().Next(101);
+   }
+   return col;
+}
+Console.Clear();
+int[] array = exp(8);
+Console.WriteLine($"{String.Join(",", array)} --> [{String.Join(",", array)}]");
---
+++ Домашняя_работа_4_урок(2)/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] exp(int size)
+{
+   int[] col = new int[size];
+   for (int i = 0; i < size; i++)
+   {
+      col[i] = new Random().Next(10);
+   }
+   return col;
+}
+
+int exp2(int[] collection)
+{
+   int sum = 0;
+   int b = collection.Length;
+   int index = 0;
+   while (index < b)
+   {
+      sum = collection[index] + sum;
+      index++;
+   }
+   return sum;
+}
+
+Console.Clear();
+int[] array = exp(3);
+int summa = exp2(array);
+Console.WriteLine($"Сумма элементов числа {String.Join("", array)} равна {summa}");

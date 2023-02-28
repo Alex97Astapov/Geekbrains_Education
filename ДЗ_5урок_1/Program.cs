1a1ce92d63a6e9270231fa0c4710b984fafd8d9b
Aleksandr <astapov.sascha@yandex.ru>
2023-02-28 13:38:51 +0300

    Сделал первое задание к пятому уроку

---
+++ ДЗ_5урок_1/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] GetArray(int size, int MinValue, int MaxValue)
+{
+   int[] res = new int[size];
+   for (int i = 0; i < size; i++)
+   {
+      res[i] = new Random().Next(MinValue, MaxValue + 1);
+   }
+   return res;
+}
+
+int Number(int[] array)
+{
+   int count = 0;
+   foreach (var item in array)
+   {
+      if (item % 2 == 0) count++;
+   }
+   return count;
+}
+
+int[] startArray = GetArray(5, 101, 999);
+Console.WriteLine($"В массиве {String.Join(",", startArray)} {Number(startArray)} четных чисел");
---
+++ Примеры/Семинары/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] Array(int[] mas)
+{
+   for (int i = 0; i < mas.Length; i++)
+   {
+      if (mas[i] < 0)
+      {
+         mas[i] = mas[i] * (-1);
+      }
+      if (mas[i] > 0)
+      {
+         mas[i] = mas[i] * (-1);
+      }
+   }
+   return mas;
+}
+
+int[] array = { 1, 2, -3, 4, -5 };
+Console.WriteLine($"[{Array(array)}]");

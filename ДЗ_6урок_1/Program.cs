bd557e22d7da67e36bbc48a04b45482b18a21dbc
Aleksandr <astapov.sascha@yandex.ru>
2023-03-05 16:39:59 +0300

    Сделал первое задание к 6 уроку

---
+++ ДЗ_6урок_1/Program.cs
@@ -0,0 +1,19 @@
+﻿int Numbers(string[] array)
+{
+   int res = 0;
+   int[] array1 = new int[array.Length];
+   for (int i = 0; i < array1.Length; i++)
+   {
+      int a = int.Parse(array[i]);
+      array1[i] = a;
+      if (array1[i] > 0)
+      {
+         res++;
+      }
+   }
+   return res;
+}
+Console.Clear();
+Console.WriteLine("Введите несколько чисел через пробел: ");
+string[] st = Console.ReadLine().Split(' ');
+Console.WriteLine(Numbers(st));
--- Примеры/Семинары/Program.cs
+++ Примеры/Семинары/Program.cs
@@ -1,18 +1,4 @@
-﻿int[] Array(int[] mas)
-{
-   for (int i = 0; i < mas.Length; i++)
-   {
-      if (mas[i] < 0)
-      {
-         mas[i] = mas[i] * (-1);
-      }
-      if (mas[i] > 0)
-      {
-         mas[i] = mas[i] * (-1);
-      }
-   }
-   return mas;
-}
-
-int[] array = { 1, 2, -3, 4, -5 };
-Console.WriteLine($"[{Array(array)}]");
+﻿double a = 2.5;
+double b = 3.1;
+double res = a / b;
+Console.WriteLine(res);

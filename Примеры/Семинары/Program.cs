d633bdab17a3ba9c30271c3af55dfaba75181971
Aleksandr <astapov.sascha@yandex.ru>
2023-03-19 17:32:59 +0300

    Выполнил 5 ДЗ к 8 уроку

--- Примеры/Семинары/Program.cs
+++ Примеры/Семинары/Program.cs
@@ -1,50 +1,52 @@
-﻿int[,] GetArray(int m, int n, int minValue, int maxValue)
+﻿int[,] Getarray(int m, int n, int min, int max)
 {
    int[,] res = new int[m, n];
    for (int i = 0; i < m; i++)
    {
       for (int j = 0; j < n; j++)
       {
-         res[i, j] = new Random().Next(minValue, maxValue + 1);
+         res[i, j] = new Random().Next(min, max + 1);
       }
    }
    return res;
 }
 
-int Summa(int[,] Iarray)
+
+void Elements(int[,] array)
 {
-   int sum = 0;
+   for (int i = 0; i < array.GetLength(0); i++)
+   {
+      for (int j = 0; j < array.GetLength(1); j++)
+      {
+         if (array[i, j] == 0)
+         {
+            int a = 0;
+            a = array[i, j] + 1;
+            Console.WriteLine($"0 встречается {a} раз");
+            return;
+         }
+      }
+   }
+}
+
+void PrintArray(int[,] Iarray)
+{
    for (int i = 0; i < Iarray.GetLength(0); i++)
    {
       for (int j = 0; j < Iarray.GetLength(1); j++)
       {
-         if (i == j)
-         {
-            sum = Iarray[i, j] + sum;
-         }
-      }
-   }
-   return sum;
-}
-
-void PrintArray(int[,] Inarray)
-{
-   for (int i = 0; i < Inarray.GetLength(0); i++)
-   {
-      for (int j = 0; j < Inarray.GetLength(1); j++)
-      {
-         Console.Write($"{Inarray[i, j]} ");
+         Console.Write($"{Iarray[i, j]}  ");
       }
       Console.WriteLine();
    }
 }
 
-Console.WriteLine("Введите число столбиков: ");
+Console.WriteLine("Vvedite col-vo strok: ");
 int rows = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите число строк: ");
+Console.WriteLine("Vvedite col-vo stolbikov: ");
 int columns = int.Parse(Console.ReadLine());
-int[,] array = GetArray(rows, columns, 0, 9);
-PrintArray(array);
+int[,] Array = Getarray(rows, columns, 0, 4);
+PrintArray(Array);
 Console.WriteLine();
-Console.WriteLine($"Сумма = {Summa(array)}");
+Elements(Array);
 
---
+++ Решения/Program.cs
@@ -0,0 +1,82 @@
+﻿Console.WriteLine("Введите кол-во строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбиков: ");
+int columns = int.Parse(Console.ReadLine());
+int[,] array = GetArray(rows, columns, 0, 10);
+//FillArrayRandom(array);
+PrintArray(array);
+SortToLower(array);
+Console.WriteLine();
+PrintArray(array);
+
+
+// Функция заполнения массива рандомно числами от 1 до 9
+//void FillArrayRandom(int[,] array)
+//{
+// for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//      for (int j = 0; j < array.GetLength(1); j++)
+//      {
+//         array[i, j] = new Random().Next(1, 10);
+//      }
+//   }
+//}
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+   int[,] res = new int[m, n];
+   for (int i = 0; i < m; i++)
+   {
+      for (int j = 0; j < n; j++)
+      {
+         res[i, j] = new Random().Next(min, max + 1);
+      }
+   }
+   return res;
+}
+
+// Функция сортировки элементов в строке двумерного массива, по убыванию
+void SortToLower(int[,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+   {
+      for (int j = 0; j < array.GetLength(1); j++)
+      {
+         for (int k = 0; k < array.GetLength(1) - 1; k++)
+         {
+            if (array[i, k] < array[i, k + 1])
+            {
+               int temp = array[i, k + 1];
+               array[i, k + 1] = array[i, k];
+               array[i, k] = temp;
+            }
+         }
+      }
+   }
+}
+
+// Функция вывода двумерного массива
+void PrintArray(int[,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+   {
+      for (int j = 0; j < array.GetLength(1); j++)
+      {
+         Console.Write($"{array[i, j]} ");
+      }
+      Console.WriteLine();
+   }
+}
+void Summa(int[,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+   {
+      for (int j = 0; j < array.GetLength(1); j++)
+      {
+         int sum = 0;
+         sum = array[i, j] + sum;
+         Console.WriteLine($"Сумма {i + 1} строки = {sum}");
+      }
+   }
+}
+
